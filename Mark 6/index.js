5e57501c1fc5fdb5900a57195704d3defa06cf68
Chaitya <chaitanyaasole@gmail.com>
2023-12-23 12:49:21 +0530

    Integrated The Frontend with Backend and added it to database

--- Mark 6/index.js
+++ Mark 6/index.js
@@ -1,14 +1,60 @@
-const expresss = require("express");
+const fs = require("node:fs"); // Importing fs module
+const expresss = require("express"); // Importing express
 const mongoose = require("mongoose");   // Importing mongoose
 
 const app = expresss(); // Creating an express app
 const port = process.env.PORT || 5000; // Setting up the port
 
+const User = require("./model/user") // Importing the user model
 
+// Middleware
+app.use(expresss.urlencoded({ extended: true })); // For parsing the data
+// Yeh karna hoga agar hum store kar rahe hai data
+
+app.use(expresss.static("public")); // For serving static files
+
+mongoose.connect("mongodb://127.0.0.1:27017/UserDataDB").then(() => {
+    console.log("Database connected");
+}).catch((e)=>{
+    console.log(e) // for printing the error
+    console.log("Database not connected");
+})
+// Yaha par hum mongoose ko connect karenge
+// copy and paste url from mongodb database
+// remove the localhost
+// 127.0.0.1 => add in case the localhost is not working
+// localhost:27017 => default port of mongodb
+// UserDataDB => Database name
+
+app.post("/formPost",async(req,res) => {
+    const userData = new User(req.body)
+    await userData.save() // save the data
+    res.sendFile(__dirname + '/public/thanks.html');
+})
+// Yaha par hum data ko store karenge
+// The first argument of post must be same as the form action
+// req => request
+// res => response
+// async => jab mera data store nahi hoga tab tak we aage nahi badega
 
 // Jab hum main page par honge "/"
+// Browser will request to the server
+// Server will send the file in get like res.send("Hello World") or index.html
 app.get("/", (req, res) => {
-    res.send("Hello World, Kya bolta bhidu");
+
+    // **
+    // res.send("Hello World, Kya bolta bhidu");
+    // let a = fs.readFileSync("./index.html") // read the file
+    // res.send(a.toString()) // send the file
+    // ** This section is of this youtuber
+
+    // ** The below line is of another youtuber
+    res.sendFile(__dirname + '/public/index.html');
+
+    // ** The below line is of copilot
+    // res.sendFile(path.join(__dirname, 'public', 'index.html'));
+
+
 });
 
 // Jab hum about section mai honge toh "/about"
